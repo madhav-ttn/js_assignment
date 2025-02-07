30c3ef8139a5be04d63fc465a31de3922cbf74d8
Madhav Setia <madhav.setia@tothenew.com>
2025-02-07 23:11:27 +0530

    pending questions completed

--- scripts/index.js
+++ scripts/index.js
@@ -43,3 +43,138 @@ checkAreaButton.addEventListener("click",()=>{
     const area=3.14*inputRadius*inputRadius;
     alert(`The area of circle is :${area}`);
 })
+
+
+
+const obj1={
+    name:"Madhav Setia",
+    age:22,
+    emp_details:{
+        emp_id:8197,
+        organization:"To The New",
+        location:"sec-144,Noida"
+    }
+}
+
+const obj2=Object.assign({},obj1);
+
+obj2.age=23;
+
+obj2.emp_details.location="delhi";
+
+console.log(obj2);
+console.log(obj1);
+
+const obj3=structuredClone(obj1);//do nested cloning
+
+console.log(obj1);
+
+obj2.emp_details.organization="TTN";
+
+console.log(obj1);
+console.log(obj2);
+
+
+const emp=[{
+    
+    Name:"Madhav",
+    Age:22,
+    Salary:15000,
+    DOB:"21-10-2002"
+},
+{
+    Name:"Rishabh",
+    Age:21,
+    Salary:20000,
+    DOB:"29-12-2003"
+},
+{
+    Name:"Zubair",
+    Age:28,
+    Salary:25000,
+    DOB:"01-10-1996"
+},
+{
+    Name:"Rishav",
+    Age:21,
+    Salary:5000,
+    DOB:"17-08-2003"
+},
+{
+    Name:"Raman",
+    Age:29,
+    Salary:55000,
+    DOB:"21-12-1995"
+},
+{
+    Name:"Raz",
+    Age:29,
+    Salary:500,
+    DOB:"21-12-1995"
+},
+]
+
+/****
+ filter all employees with salary greater than 5000
+ */
+
+const emp_salary_greater_5000=[];
+
+const emp_salary_less_1000_age_greater_20=[];
+for(let i=0;i<emp.length;i++){
+    if(emp[i].Salary>5000){
+        emp_salary_greater_5000.push(emp[i]);
+    }
+    else if(emp[i].Salary<1000 && emp[i].Age>20){
+        emp[i].Salary=emp[i].Salary+emp[i].Salary*5;
+        emp_salary_less_1000_age_greater_20.push(emp[i]);
+    }
+}
+
+console.log(emp_salary_greater_5000); 
+
+/*
+{Name: 'Madhav', Age: 22, Salary: 15000, DOB: '21-10-2002'}
+1
+: 
+{Name: 'Rishabh', Age: 21, Salary: 20000, DOB: '29-12-2003'}
+2
+: 
+{Name: 'Zubair', Age: 28, Salary: 25000, DOB: '01-10-1996'}
+3
+: 
+{Name: 'Raman', Age: 29, Salary: 55000, DOB: '21-12-1995'}
+length:4
+*/
+
+console.log(emp_salary_less_1000_age_greater_20);
+
+
+
+/***
+0
+: 
+Age
+: 
+29
+DOB
+: 
+"21-12-1995"
+Name
+: 
+"Raz"
+Salary
+: 
+3000
+[[Prototype]]
+: 
+Object
+length
+: 
+1
+[[Prototype]]
+: 
+Array(0)
+
+
+*/
